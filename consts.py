fef7b04e58b7b989889df08fe74fa4284b488827
filip-pr <130585602+filip-pr@users.noreply.github.com>
2023-12-10 17:29:11 +0100

    Update consts.py

--- consts.py
+++ consts.py
@@ -4,5 +4,7 @@ import os
 UNICODE_CHAR_BYTES = 3
 INT_BYTES = 4
 BOOL_BYTES = 1
+BYTE_ORDER = "big"
+NUMPY_BYTE_ORDER = ">"
 AUDIO_BYTES_PER_SAMPLE = 2
 RENDER_TEMPS_PATH = os.path.join(os.getcwd(), "charvid_render_temp")
