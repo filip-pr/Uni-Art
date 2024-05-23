6291673f6e3e2eacae7f57ab74f2d34684019a96
Filip Prášil <filipprasil123@gmail.com>
2024-05-24 00:02:32 +0200

    Update module __init__.py

--- media2text/__init__.py
+++ media2text/__init__.py
@@ -2,3 +2,8 @@
 
 from .image_query_font import ImageQueryFont
 from .image_convert import image_convert
+from .video_convert import TextVideoPlayer
+from .helpers import ready_render_temp, benchmark
+
+
+ready_render_temp()
